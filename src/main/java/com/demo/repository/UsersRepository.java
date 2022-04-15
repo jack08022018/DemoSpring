2fd5e6d3f5155e00cb16b3490ff28a0d531e2ca7
dinhhoang <dinhtienhoang2009@gmail.com>
2022-04-15 11:26:55 +0700

    aa

---
+++ src/main/java/com/demo/repository/UsersRepository.java
@@ -0,0 +1,15 @@
+package com.demo.repository;
+
+import com.demo.dto.ProductDto;
+import com.demo.dto.UsersEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
+
+}
